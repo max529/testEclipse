63ffd438812b24bac7647a15607c92b20587a356
Maxime Bétrisey <maxime.betrisey@hotmail.com>
2018-03-10 10:27:16 +0100

    first commit

---
+++ src/main/Main.java
@@ -0,0 +1,17 @@
+/**
+ * Exercise 632-1, 
+ * week X, 
+ * question X
+ * Author : Maxime Bétrisey
+ * Date creation : 10 mars 2018
+ */
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
